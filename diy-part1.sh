19d328ed029997e6008ea54b42e8d7fb0ded9210
rianjskis <86937445+rianjskis@users.noreply.github.com>
2023-09-15 17:50:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-git clone --depth=1 https://github.com/linkease/istore.git package/istore
-git clone https://github.com/sbwml/luci-app-alist package/alist
-git clone https://github.com/kenzok78/luci-app-fileassistant package/luci-app-fileassistant
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone https://github.com/natelol/luci-app-beardropper package/luci-app-beardropper
