b50a933b527354d9cf06fe5731c743e98a7d1e0c
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-11-10 09:54:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,9 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate     #更换初始IP
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile   # 选择argon为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile   # 选择argon为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile   # 选择argon为默认主题
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
-rm -rf feeds/luci/themes/luci-theme-bootstrap  # 删除默认bootstrap主题
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
